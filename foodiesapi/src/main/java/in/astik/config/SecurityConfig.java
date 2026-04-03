829f6759cba490bf77cc3f30c867a09f37356fec
Astik Yadav <astikyadav4@gmail.com>
2026-04-04 01:07:10 +0530

    feat: implement Spring Security configuration with JWT authentication and CORS support

--- foodiesapi/src/main/java/in/astik/config/SecurityConfig.java
+++ foodiesapi/src/main/java/in/astik/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
 		http
 			.cors(Customizer.withDefaults())
 			.csrf(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(auth->auth.requestMatchers("/api/register","/api/login","/api/foods/**","/images/**").permitAll().anyRequest().authenticated())
+			.authorizeHttpRequests(auth->auth.requestMatchers("/api/register","/api/login","/api/foods", "/api/foods/**","/images/**").permitAll().anyRequest().authenticated())
 			.sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 		return http.build();
