7a2f65b124ad1f5a72e462897d9b351fe137e474
Astik Yadav <astikyadav4@gmail.com>
2026-09-04 23:28:43 +0530

    deployement url

--- adminpanel/src/services/foodService.jsx
+++ adminpanel/src/services/foodService.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api/foods";
+const API_URL = "https://foodies-api-cl26.onrender.com/api/foods";
 
 const addFood = async (foodData, image) => {
   const formData = new FormData();
--- foodies/src/service/authService.js
+++ foodies/src/service/authService.js
@@ -1,20 +1,11 @@
 import axios from "axios";
-const API_URL = "http://localhost:8080/api";
 
-export const registerUser=async(data)=>{
-    try {
-        const res=await axios.post(API_URL+"/register",data)
-        return res
-    } catch (error) {
-        throw error
-    }
-}
+const API_URL = "https://foodies-api-cl26.onrender.com/api";
 
-export const login=async(data)=>{
-    try {
-        const res=await axios.post(API_URL+"/login",data)
-        return res
-    } catch (error) {
-        throw error
-    }
-}
+export const registerUser = async (data) => {
+    return await axios.post(`${API_URL}/register`, data);
+};
+
+export const login = async (data) => {
+    return await axios.post(`${API_URL}/login`, data);
+};
